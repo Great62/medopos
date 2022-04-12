02a2fe7e076c0c7e980aef7bc11badba6f791cbf
Adam <Adamkindel1234@gmail.com>
2022-04-12 20:51:59 +0200

    phone update

--- src/Components/CommonCases.js
+++ src/Components/CommonCases.js
@@ -1,19 +1,20 @@
 import "../Styles/healthlists.css";
 import React, { useEffect, useState } from "react";
 import { commonCases } from "../Data";
+import operation from "../Pictures/chirurgie-robotique.png";
 
 function CommonCases() {
   return (
-    <div className="common-cases-container">
-      <h3 className="list-title">Common cases</h3>
-      <div className="health-list">
-        {commonCases.map((name, key) => (
-          <div className="gradient-wrap-border-blue" key={key}>
-            <div className="health-list-item">
+    <div className="health-list-container">
+      <div className="common-cases-container">
+        <h3 className="list-title">Common cases</h3>
+        <div className="health-list">
+          {commonCases.map((name, key) => (
+            <div className="health-list-item" key={key}>
               <div className="health-list-item-text">{name}</div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/Components/NHSWaitingList.js
+++ src/Components/NHSWaitingList.js
@@ -1,19 +1,20 @@
 import "../Styles/healthlists.css";
 import { nhsWaitingLists } from "../Data";
 import React from "react";
+import hospital from "../Pictures/batiment-de-lhopital.png";
 
 function NHSWaitingList() {
   return (
-    <div className="NHS-waiting-list">
-      <h3 className="list-title">NHS waiting lists</h3>
-      <div className="health-list">
-        {nhsWaitingLists.map((name, key) => (
-          <div className="gradient-wrap-border-red" key={key}>
-            <div className="health-list-item-nhs">
+    <div className="health-list-container">
+      <div className="NHS-waiting-list">
+        <h3 className="list-title">NHS waiting lists</h3>
+        <div className="health-list">
+          {nhsWaitingLists.map((name, key) => (
+            <div className="health-list-item-nhs" key={key}>
               <div className="health-list-item-text">{name}</div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
