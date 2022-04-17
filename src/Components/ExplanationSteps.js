b546a13680be982749868a1f68f7b604c3638b8e
Adam <Adamkindel1234@gmail.com>
2022-04-17 13:25:25 +0200

    steps titles

--- src/Components/ExplanationSteps.js
+++ src/Components/ExplanationSteps.js
@@ -10,7 +10,7 @@ function ExplanationSteps() {
       <div className="step">
         <img src={call} alt="" className="step-picture picture-lower" />
         <div className="step-text">
-          <h3 className="step-title">Lorem, ipsum.</h3>
+          <h3 className="step-title">Give us a call</h3>
           <h4 className="step-explained">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. In est
             exercitationem totam commodi ullam deleniti molestias facilis non at
@@ -20,7 +20,7 @@ function ExplanationSteps() {
       </div>
       <div className="step">
         <div className="step-text">
-          <h3 className="step-title">Lorem, ipsum.</h3>
+          <h3 className="step-title">Go to your destination</h3>
           <h4 className="step-explained">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. In est
             exercitationem totam commodi ullam deleniti molestias facilis non at
@@ -32,7 +32,7 @@ function ExplanationSteps() {
       <div className="step">
         <img src={treatment} alt="" className="step-picture" />
         <div className="step-text">
-          <h3 className="step-title">Lorem, ipsum.</h3>
+          <h3 className="step-title">Get treated</h3>
           <h4 className="step-explained">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. In est
             exercitationem totam commodi ullam deleniti molestias facilis non at
