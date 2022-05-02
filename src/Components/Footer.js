2f64db66d9c80514aceb0dcf2547c7c5b789b651
Adam <Adamkindel1234@gmail.com>
2022-05-02 08:32:11 +0200

    sitemap footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,28 @@
 import React from "react";
+import "../Styles/footer.css";
 
 function Footer() {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <div className="page-links-container">
+        <a href="/" className="page-link">
+          Home
+        </a>
+        <a href="https://medopos.vercel.app/UsAndYou" className="page-link">
+          Us and You
+        </a>
+        <a href="/FAQs" className="page-link">
+          FAQs
+        </a>
+        <a href="https://medopos.vercel.app/GoingPrivate" className="page-link">
+          Going private
+        </a>
+        <a href="https://medopos.vercel.app/Legals" className="page-link">
+          Legals
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
